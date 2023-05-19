bea57a66446aaf039a3a48e167ba1dab28b5fee8
Dorian Margraf <dorian.margraf@gmail.com>
2023-05-19 14:22:04 +0200

    Added Custom Pipe, Filtering function

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,18 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component ({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+
   pageTitle: string = 'Product List';
   imageWidth = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
 
-  products: any[] = [
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  filteredProducts: IProduct[] = [];
+ 
+  products: IProduct[] = [
       {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -65,7 +78,17 @@ export class ProductListComponent {
       }
   ];
 
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLowerCase().includes(filterBy));
+  }
+
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('In OnInit');
   }
 }
