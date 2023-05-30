b9bfae155ddac148c6dd8c07ffa5160fca26afdb
Dorian Margraf <dorian.margraf@gmail.com>
2023-05-30 14:09:40 +0200

    Added productService and registered in root application injector

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,32 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
-import { ProductService } from "./product.service";
 
-@Component ({
-  selector: 'pm-products',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+@Injectable({
+  providedIn: 'root'
 })
-
-export class ProductListComponent implements OnInit{
-
-  pageTitle: string = 'Product List';
-  imageWidth = 50;
-  imageMargin: number = 2;
-  showImage: boolean = false;
-
-  private _listFilter: string = '';
-  get listFilter(): string {
-    return this._listFilter;
-  }
-  set listFilter(value: string) {
-    this._listFilter = value;
-    this.filteredProducts = this.performFilter(value);
-  }
-
-  filteredProducts: IProduct[] = [];
- 
-  products: IProduct[] = [
+export class ProductService {
+  getProducts(): IProduct[] {
+    return [
       {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -77,26 +57,6 @@ export class ProductListComponent implements OnInit{
         "starRating": 4.6,
         "imageUrl": "assets/images/xbox-controller.png"
       }
-  ];
-
-  constructor(private productService: ProductService) {}
-
-  performFilter(filterBy: string): IProduct[] {
-    filterBy = filterBy.toLowerCase();
-    return this.products.filter((product: IProduct) =>
-      product.productName.toLowerCase().includes(filterBy));
-  }
-
-  toggleImage(): void {
-    this.showImage = !this.showImage;
-  }
-
-  ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
-  }
-
-  onRatingClicked(message: string): void {
-    this.pageTitle = 'Product list -' + message;
+    ]
   }
 }
